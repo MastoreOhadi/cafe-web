cf73e7800a4fde5d4eec641fc7cc5520716b67f5
rezatg <yousudo@outlook.com>
2025-09-27 10:44:57 +0330

    feat: add routing for cafe page and auth pages
    Integrate lazy loading for cafe and authentication components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,25 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
    {
-      title: "signup",
-      path: "signup",
-      loadComponent: () =>
-         import("./modules/auth/pages/register/register").then(c => c.Register)
-   }, {
-      title: "login",
-      path: "login",
-      loadComponent: () =>
-         import("./modules/auth/pages/login/login").then(c => c.Login)
-   }, {
       title: "page",
       path: "page",
       loadComponent: () =>
          import("./modules/cafe/pages/cafe-page/cafe-page").then(c => c.CafePage)
+   }, {
+      title: "auth",
+      path: "auth",
+      children: [
+         {
+            title: "signup",
+            path: "signup",
+            loadComponent: () =>
+               import("./modules/auth/pages/register/register").then(c => c.Register)
+         }, {
+            title: "login",
+            path: "login",
+            loadComponent: () =>
+               import("./modules/auth/pages/login/login").then(c => c.Login)
+         }
+      ]
    }
 ];
